02b9b9ca8e0b04a5ec3a9f4e0fac03ff013076f1
hsky0 <1415561000@qq.com>
2024-03-31 22:16:26 +0800

    vlab-htl

---
+++ Algorithm/java/class01/Code01_SelectionSort.java
@@ -0,0 +1,12 @@
+
+public class Code01_SelectionSort {
+    
+    public static void selectionSort(int[] arr){
+        if(arr == null || arr.length < 2) {
+            return;
+        }
+        for(int i = 0; i < arr.length - 1; i++){
+
+        }
+    }
+}
